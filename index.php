6e00e088cb2e449043db76ebcb689c55e83abd64
reenuchahal <reenuchahal@gmail.com>
2013-09-12 09:47:56 -0400

    index.php added

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Reenu Chahal | CSCIE-15 | Dynamic Web Application | Harvard Extension School </title>
+<link rel="shortcut icon" href="images/favicon.ico" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link href='http://fonts.googleapis.com/css?family=Quattrocento:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="container">
+<header class="row">
+<div class="col-sm-6 col-lg-6 pageHeading">
+<h1>Reenu Chahal</h1>
+<h3>CSCI E-15 Dynamic Web Applications</h3>
+<h4>Project 1</h4>
+</div>
+
+</header>
+
+<div class="row mainContent">
+ <aside class="col-sm-4 col-lg-4 ">
+  <h3 class="text-center">My Skills</h3>
+  <ul class="nav nav-pills">
+  <li><a href="#">Joomla </a></li>
+  <li><a href="#">Bootstrap </a></li>
+  <li><a href="#">HTML </a></li>
+  <li><a href="#">CSS </a></li>
+  <li><a href="#">XML / XSLT </a></li>
+  <li><a href="#">Git </a></li>
+  <li><a href="#">Responsive Layouts</a></li>
+  </ul>
+  <hr class="hidden-sm hidden-md hidden-lg" />
+  <h3 class="text-center">Familiarized with </h3>
+   <ul class="nav nav-pills">
+  <li><a href="#">JAVA </a></li>
+  <li><a href="#">PHP </a></li>
+  <li><a href="#">jQuery </a></li>
+  <li><a href="#">Javascript</a></li>
+  <li><a href="#">Mysql </a></li>
+  <li><a href="#">Coldfusion </a></li>
+  <li><a href="#">Oracle</a></li>
+  </ul>
+   <hr class="hidden-sm hidden-md hidden-lg" />
+   </aside>
+ <article class="col-sm-8 col-lg-8">
+ <h2>About me</h2>
+ <p>I grew up in India where I went to SNDT University and majored in Textile designing. In Dec 2008, I moved to US.
+I enrolled in a web design course at Santa Barbara City College to build a textile design portfolio. 
+ From very after day I found that course extremely interested and realized that I could combine my designing skills and creativity with IT field to produce interesting results. I completed few courses basic to advance in web design from SBCC. 
+ </p>
+
+ <p> In 2010, I moved to Boston. Here I enrolled myself in Web Technologies Certificate at  Harvard Extension School. Besides this I worked as a web intern and a freelance designer and developer, built static to dynamic websites using different technologies.  Afterwards I started my career at USpin LLC as a Front-End Engineer. I work hard and pay attention to details. And outside of work, I love to spend time with my family and friends. 
+</p>
+<p>
+ <strong>*** Note: Resize your browser window to check the responsive layout of this page.</strong>
+</p>
+ </article>
+  
+ 
+</div>
+<footer class="row">
+
+<div class="col-md-offset-6 col-sm-6 col-lg-6 pageFooter">
+<p>copyright&#169;<a href="http://reenuchahal.info">reenuchahal.info 2013</a> <br/> <a href="mailto:reenuchahal@fas.harvard.edu">reenuchahal@fas.harvard.edu</a></p>
+<a class="fb" href="https://www.facebook.com/reenu.chahal.3"><img src="images/fb.png" alt="Facebook" /></a>
+<a class="twitter"  href="https://twitter.com/reenuchahal"><img src="images/twitter.png" alt="Twitter"/></a>
+<a class="linkedin"  href="http://www.linkedin.com/profile/view?id=46306550&amp;trk=nav_responsive_tab_profile"><img src="images/linkedin.png" alt="LinkedIn"/></a>
+</div>
+</footer>
+
+</div>
+<!-- Scripts -->
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Scripts Ends -->
+</body>
+</html>
